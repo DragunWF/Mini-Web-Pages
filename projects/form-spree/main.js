e998fbf9596a9ab8e64ae0a95155e53b82c79104
DragonWF <plarisanmarc@gmail.com>
2022-01-25 20:07:01 +0800

    Added main functionality for forms

--- projects/form-spree/main.js
+++ projects/form-spree/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-    
-})
+  const hiddenContents = [".content", ".container"];
+
+  for (content of hiddenContents) {
+    $(content).hide();
+  }
+
+  $("#submit").click(() => {
+    if ($("#input-textarea").val()) {
+      if (!$("#input-name").val()) $("#input-name").val("Anonymous");
+      $("#submit").attr("type", "submit");
+    } else alert("Please enter your message");
+  });
+
+  $("#reset").click(() => {
+    $("#input-name").val("");
+    $("#input-textarea").val("");
+  });
+
+  $(".content").slideDown(1500, () => {
+    $(".container").fadeIn(1250);
+  });
+});
